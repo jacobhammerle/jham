712565314ca6848ccd89addc2c782bd30caeae74
Jacob Hammerle <jacob@hammerle.us>
2020-09-29 20:17:32 -0400

    update about page and add new slushie project

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,7 +3,8 @@
         <div class="pt-32 pb-32">
             <div class="max-w-xl mx-4">
                 <h1 class="text-3xl mb-12">Projects</h1>
-                <GitHub project-title="(Name Pending)" repo="note-taking-app"/>
+                <GitHub project-title="Slushie" repo="slushie"/>
+                <GitHub project-title="Note Taking App" repo="note-taking-app"/>
                 <GitHub project-title="My Personal Website" repo="jham"/>
                 <Twitter />
             </div>
